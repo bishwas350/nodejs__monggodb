531c61a113c1ee80c9fe2e0afa5442daba8486d6
bishwas350 <crashmindboy10@gmail.com>
2025-09-21 19:39:35 +0600

    Add user model schema with validation and default values

---
+++ src/models/user.model.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+const {Types, Schema} = mongoose;
+const userSchema = new Schema({
+    name : {
+        type : String,
+        trim : true,
+    },
+    email :{
+        type : String,
+        trim : true,
+        required :[true, "Email is required"],
+    },
+    password : {
+        type : String,
+        trim : true,
+        required :[true, "Password is required"],
+    },
+    phoneNumber :{
+        type: Number,
+        trim : true,
+        required :[true, "Phone number is required"],
+    },
+    image :{
+        type : String,
+        trim : true,
+    },
+    emailVerified :{
+        type : Boolean,
+        default : false,
+    },
+    phoneNumberVerified :{
+        type : Boolean,
+        default : false,
+    },
+    role :{
+        type:types.ObjectId,
+        ref : "role",
+
+    },
+    permission :{
+        type:Types.ObjectId,
+        ref : "permission",
+    },
+    address:{
+        type : String,
+        trim : true,
+    },
+    city: String,
+    district: String,
+    country:{
+        type : String,
+        default : "Bangladesh"
+    },
+    zipCode:{
+        type: Number,
+        trim : true,
+    },
+    dateOfBirth:{
+        type : Date,
+        trim : true,
+    },
+    gender:{
+        type : String,
+        emun: ["Men","Female","Other"] 
+    },
+    cart:{
+        type:Types.ObjectId,
+        ref : "Product",
+    },
+    wishlist:{
+        type:Types.ObjectId,
+        ref : "Product",
+    },
+    newsletterSubscribed:{
+        type : Boolean,
+        default : false,
+    },
+    resetPasswordOtp:Number,
+    resetPasswordOtpExpire:Date,
+    twoFactorAuthEnabled:{
+    type : Boolean,
+        default : false,
+    },
+    isBlocked:{
+    type : Boolean,
+        default : false,
+    },
+    isActive:{
+    type : Boolean,
+        default : false,
+    },
+    lastLogIn:Date,
+    lastLogOut:Date,
+    oauth:Boolean,
+    refreshToken:{
+        type : String,
+        trim : true,
+    }
+})
+module.exports = mongoose.model("User", userSchema);
