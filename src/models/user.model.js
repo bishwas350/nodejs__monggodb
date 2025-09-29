a6dca7acd1ff5223d6c8d42d380b5d3f714b4d5f
bishwas350 <crashmindboy10@gmail.com>
2025-09-30 01:36:15 +0600

    Refactor user model and validation schema: improve formatting, add phone number validation, and enforce password complexity requirements

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,145 +1,154 @@
 const mongoose = require("mongoose");
-const {Types, Schema} = mongoose;
-const bcrypt = require("bcrypt")
+const { Types, Schema } = mongoose;
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const coustomError = require("../../utils/coustomError");
 const userSchema = new Schema({
-    name : {
-        type : String,
-        trim : true,
-    },
-    email :{
-        type : String,
-        trim : true,
-        required :[true, "Email is required"],
-    },
-    password : {
-        type : String,
-        trim : true,
-        required :[true, "Password is required"],
-    },
-    phoneNumber :{
-        type: Number,
-        trim : true,
-        required :[true, "Phone number is required"],
-    },
-    image :{
-        type : String,
-        trim : true,
-    },
-    emailVerified :{
-        type : Boolean,
-        default : false,
-    },
-    phoneNumberVerified :{
-        type : Boolean,
-        default : false,
-    },
-    role :{
-        type:Types.ObjectId,
-        ref : "role",
-
-    },
-    permission :{
-        type:Types.ObjectId,
-        ref : "permission",
-    },
-    address:{
-        type : String,
-        trim : true,
-    },
-    city: String,
-    district: String,
-    country:{
-        type : String,
-        default : "Bangladesh"
-    },
-    zipCode:{
-        type: Number,
-        trim : true,
-    },
-    dateOfBirth:{
-        type : Date,
-        trim : true,
-    },
-    gender:{
-        type : String,
-        emun: ["Men","Female","Other"] 
-    },
-    cart:{
-        type:Types.ObjectId,
-        ref : "Product",
-    },
-    wishlist:{
-        type:Types.ObjectId,
-        ref : "Product",
-    },
-    newsletterSubscribed:{
-        type : Boolean,
-        default : false,
-    },
-    resetPasswordOtp:Number,
-    resetPasswordOtpExpire:Date,
-    twoFactorAuthEnabled:{
-    type : Boolean,
-        default : false,
-    },
-    isBlocked:{
-    type : Boolean,
-        default : false,
-    },
-    isActive:{
-    type : Boolean,
-        default : false,
-    },
-    lastLogIn:Date,
-    lastLogOut:Date,
-    oauth:Boolean,
-    refreshToken:{
-        type : String,
-        trim : true,
-    }
-})
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: [true, "Password is required"],
+  },
+  phoneNumber: {
+    type: Number,
+    trim: true,
+    required: [true, "Phone number is required"],
+  },
+  image: {
+    type: String,
+    trim: true,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  phoneNumberVerified: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: Types.ObjectId,
+    ref: "role",
+  },
+  permission: {
+    type: Types.ObjectId,
+    ref: "permission",
+  },
+  address: {
+    type: String,
+    trim: true,
+  },
+  city: String,
+  district: String,
+  country: {
+    type: String,
+    default: "Bangladesh",
+  },
+  zipCode: {
+    type: Number,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    emun: ["Men", "Female", "Other"],
+  },
+  cart: {
+    type: Types.ObjectId,
+    ref: "Product",
+  },
+  wishlist: {
+    type: Types.ObjectId,
+    ref: "Product",
+  },
+  newsletterSubscribed: {
+    type: Boolean,
+    default: false,
+  },
+  resetPasswordOtp: Number,
+  resetPasswordOtpExpire: Date,
+  twoFactorAuthEnabled: {
+    type: Boolean,
+    default: false,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  lastLogIn: Date,
+  lastLogOut: Date,
+  oauth: Boolean,
+  refreshToken: {
+    type: String,
+    trim: true,
+  },
+});
 
 // make a hash password
 userSchema.pre("save", async function (next) {
-    if(this.isModified("password")){
-        const hashPass = await bcrypt.hash(this.password, 10)
-        this.password = hashPass
-    }
-    next()
-})
+  if (this.isModified("password")) {
+    const hashPass = await bcrypt.hash(this.password, 10);
+    this.password = hashPass;
+  }
+  next();
+});
 
 // compare password
-userSchema.methods.comparePassword = async function( humanPass){
-return await bcrypt.compare(humanPass , this.password)
-}
+userSchema.methods.comparePassword = async function (humanPass) {
+  return await bcrypt.compare(humanPass, this.password);
+};
 
 // generate access token
-userSchema.methods.generateAccessToken = async function(){
-  const accesstoken = await jwt.sign({
-  userId : this._id,
-    email : this.email,
-    role : this.role,
-    name : this.name,
-}, process.env.ACCESSTOKEN_SECRET_KEY, { expiresIn: process.env.ACCESSTOKEN_EXPIRE});
- return accesstoken
-}
+userSchema.methods.generateAccessToken = async function () {
+  const accesstoken = await jwt.sign(
+    {
+      userId: this._id,
+      email: this.email,
+      role: this.role,
+      name: this.name,
+    },
+    process.env.ACCESSTOKEN_SECRET_KEY,
+    { expiresIn: process.env.ACCESSTOKEN_EXPIRE }
+  );
+  return accesstoken;
+};
 // generate refresh token
-userSchema.methods.generateAccessToken = async function(){
-  return await jwt.sign({
-  userId : this._id,
-}, process.env.REFRESHTOKEN_SECRET_KEY, { expiresIn: process.env.REFRESHTOKEN_EXPIRE});
- return accesstoken
-}
+userSchema.methods.generateAccessToken = async function () {
+  return await jwt.sign(
+    {
+      userId: this._id,
+    },
+    process.env.REFRESHTOKEN_SECRET_KEY,
+    { expiresIn: process.env.REFRESHTOKEN_EXPIRE }
+  );
+  return accesstoken;
+};
 
 // verify access token
-userSchema.methods.verifyAccessToken = async function(token){
-     const isValiedRefreshToken = await jwt.verify(token, process.env.REFRESHTOKEN_SECRET_KEY);
-    if(!isValiedRefreshToken){
-        throw new coustomError(401, "Invalid Refresh Token")
-    }
-}
-
+userSchema.methods.verifyAccessToken = async function (token) {
+  const isValiedRefreshToken = await jwt.verify(
+    token,
+    process.env.REFRESHTOKEN_SECRET_KEY
+  );
+  if (!isValiedRefreshToken) {
+    throw new coustomError(401, "Invalid Refresh Token");
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -1,23 +1,51 @@
 const joi = require("joi");
-const userValidationSchema = joi.object({
-    email:joi.string().trim().required().pattern( /^[^\s@]+@[^\s@]+\.[^\s@]+$/).message({
-        'string.emtpy': "email faka rakhha jabe na",
-        'string.pattern.base': "email format ta thik na",
-        'any.required': "email ta dorkar", 
-    }),
-    password:joi.string().trim().required().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).message({
-        'string.emtpy': "password faka rakhha jabe na",
-        'string.pattern.base': "password format ta thik na",
-        'any.required': "password ta dorkar",
-    })
-})
+const coustomError = require("../../utils/coustomError");
 
-exports.userValidation = async(req)=>{
-    try {
-      const value =  await userValidationSchema.validateAsync(req.body)
-      return value
-    } catch (error) {
-        console.log("error from user validation", error);
-        throw new coustomError(401, value)
-    }
-}
+const userValidationSchema = joi.object(
+  {
+    email: joi
+      .string()
+      .trim()
+      .required()
+      .pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+      .messages({
+        "string.empty": "email faka rakhha jabe na",
+        "string.pattern.base": "email format ta thik na",
+        "any.required": "email ta dorkar",
+      }),
+    password: joi
+      .string()
+      .trim()
+      .required()
+      .min(8)
+      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)
+      .messages({
+        "string.empty": "password faka rakhha jabe na",
+        "string.pattern.base": "password format ta thik na",
+        "any.required": "password ta dorkar",
+      }),
+    phoneNumber: joi
+      .string()
+      .empty()
+      .trim()
+      .pattern(/^1[3-9]\d{8}$/, "Bangladesh phone number")
+      .messages({
+        "string.empty": "phone number faka rakhha jabe na",
+        "string.pattern.base": "phone number format ta thik na",
+        "string.name": "invalid phone number",
+      }),
+  },
+  {
+    allowUnknown: true,
+  }
+);
+
+exports.validateUser = async (req) => {
+  try {
+    const value = await userValidationSchema.validateAsync(req.body);
+    return value;
+  } catch (error) {
+    console.log("error from user validation", error);
+    throw new coustomError(401, error.message);
+  }
+};
