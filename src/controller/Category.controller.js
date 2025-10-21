648f16d7980cedcebf7f0668d9906f2783bb1b23
bishwas350 <crashmindboy10@gmail.com>
2025-10-21 09:58:32 +0600

    Add Cloudinary integration for image uploads and update category creation logic

--- src/controller/Category.controller.js
+++ src/controller/Category.controller.js
@@ -1,11 +1,18 @@
 const apiResponse = require("../../utils/apiResponse");
 const { asyncHandler } = require("../../utils/asyncHandler");
 const coustomError = require("../../utils/coustomError");
+const { uploadCloudinaryFile } = require("../helpers/Cloudinary");
 const CategoryModel = require("../models/Category.model");
 const { categoryValidationSchema, validateCategory} = require("../validation/Category.validation");
 
 // create a category
 exports.createCategory = asyncHandler(async (req,res)=>{
     const value = await validateCategory(req)
-    console.log(value)
+    const imgUrl =await uploadCloudinaryFile(value?.image?.path)
+    const category = await new CategoryModel({
+        name:value.name,
+        image:imgUrl
+    }).save()
+    if(!category) throw new coustomError(400,"Category create failed")
+    apiResponse.sendSusses(res,201,"Category created successfully",category)
 })
---
+++ src/helpers/Cloudinary.js
@@ -0,0 +1,26 @@
+const coustomError = require('../../utils/coustomError');
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: 'dpminbaw5', 
+  api_key: '333942862231742', 
+  api_secret: 'NnOysFey0EDOMNLLCA8ezEAOvQk'
+});
+
+exports.uploadCloudinaryFile =async (filePath)=>{
+    try {
+        const result =await cloudinary.uploader.upload(filePath, {
+            auto:"format",
+            quality:"auto",
+            fetch_format:"auto",
+            resource_type:"image"
+        });
+        //now optimize the img
+        const url = await cloudinary.url(result.public_id,{resource_type: "Image"})
+        fs.unlinkSync(filePath) // delete the local file after upload
+        return url;
+    } catch (error) {
+        throw new coustomError(400,error.message)
+    }
+}
