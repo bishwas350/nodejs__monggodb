43d2daa89042dbc631a012ce4f9e283a7d3679fa
bishwas350 <crashmindboy10@gmail.com>
2025-10-25 19:20:08 +0600

    Refactor category controller and API routes for improved readability and maintainability

--- src/controller/Category.controller.js
+++ src/controller/Category.controller.js
@@ -3,45 +3,65 @@ const { asyncHandler } = require("../../utils/asyncHandler");
 const coustomError = require("../../utils/coustomError");
 const { uploadCloudinaryFile } = require("../helpers/Cloudinary");
 const CategoryModel = require("../models/Category.model");
-const { categoryValidationSchema, validateCategory} = require("../validation/Category.validation");
+
+
+const {
+  categoryValidationSchema,
+  validateCategory,
+} = require("../validation/Category.validation");
+
+
 
 // create a category
-exports.createCategory = asyncHandler(async (req,res)=>{
-    const value = await validateCategory(req)
-    const imgUrl =await uploadCloudinaryFile(value?.image?.path)
-    const category = await new CategoryModel({
-        name:value.name,
-        image:imgUrl
-    }).save()
-    if(!category) throw new coustomError(400,"Category create failed")
-    apiResponse.sendSusses(res,201,"Category created successfully",category)
-})
+exports.createCategory = asyncHandler(async (req, res) => {
+  const value = await validateCategory(req);
+  const imgUrl = await uploadCloudinaryFile(value?.image?.path);
+  const category = await new CategoryModel({
+    name: value.name,
+    image: imgUrl,
+  }).save();
+  if (!category) throw new coustomError(400, "Category create failed");
+  apiResponse.sendSusses(res, 201, "Category created successfully", category);
+});
 
-exports.getAllCategory = asyncHandler(async(req,res)=>{
-    const category =await CategoryModel.find(). sort({createdAt:-1})
-    if(!category) throw new coustomError(500,"Category not found")
-        apiResponse.sendSusses(res,200,"Category fetch successfully",category)
-})
+
+
+exports.getAllCategory = asyncHandler(async (req, res) => {
+  const category = await CategoryModel.find().sort({ createdAt: -1 });
+  if (!category) throw new coustomError(500, "Category not found");
+  apiResponse.sendSusses(res, 200, "Category fetch successfully", category);
+});
+
+
 
 //find a single category by slug
-exports.singleCategory =asyncHandler(async(req,res)=>{
-    const {slug} = req.params;
-    const category = await CategoryModel.findOne({slug})
-    if(!category) throw new coustomError(404,"Category not found")
-    apiResponse.sendSusses(res,200,"Category fetch successfully",category)
-})
+exports.singleCategory = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  const category = await CategoryModel.findOne({ slug });
+  if (!category) throw new coustomError(404, "Category not found");
+  apiResponse.sendSusses(res, 200, "Category fetch successfully", category);
+});
+
+
+
 //update category
-exports.updateCategory = asyncHandler(async(req,res)=>{
-    const {slug}= req.params
-if(!slug) throw new coustomError(400,"slug missing")
-    // find category
-const category = await CategoryModel.findOne({slug:slug})
-if(!category) throw new coustomError(500 ,  "category not found")
-category.name = req.body?.name || category.name
-if(req.files.image){
-    const imageurl = await uploadCloudinaryFile (req?.files?.image[0]?.path)
-    category.image = imageurl
-}
-await category.save()
-apiResponse.sendSusses(res,200,"Category updated successfully",category)
-})
+exports.updateCategory = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  if (!slug) throw new coustomError(400, "slug missing");
+
+
+  // find category
+  const category = await CategoryModel.findOne({ slug: slug });
+  if (!category) throw new coustomError(500, "category not found");
+
+  if(req.body.name){
+    category.name = req.body?.name ;
+  }
+  
+  if (req.files.image) {
+    const imageurl = await uploadCloudinaryFile(req?.files?.image[0]?.path);
+    category.image = imageurl;
+  }
+  await category.save();
+  apiResponse.sendSusses(res, 200, "Category updated successfully", category);
+});
--- src/routes/api/category.api.js
+++ src/routes/api/category.api.js
@@ -2,7 +2,11 @@ const express = require("express");
 const _ = express.Router();
 const categoryController = require("../../controller/Category.controller");
 const upload = require("../../middlewere/multer");
-_.route("/create_category").post(upload.fields([{name:"image",maxCount:1}]),categoryController.createCategory);
+_.route("/create_category").post(
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  categoryController.createCategory
+);
 _.route("/get_all_category").get(categoryController.getAllCategory);
-_.route('/singleCategory/:slug').get(categoryController.singleCategory)
+_.route("/singleCategory/:slug").get(categoryController.singleCategory);
+_.route("/updateCategory/:slug").put(upload.fields([{ name: "image", maxCount: 1 }]),categoryController.updateCategory);
 module.exports = _;
