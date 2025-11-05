fbd7cc939bc869397c2a6f7301b958c64bd477ad
bishwas350 <crashmindboy10@gmail.com>
2025-11-06 01:10:22 +0600

    Add category model updates on subcategory creation and deletion

--- src/controller/SubCategory.Controller.js
+++ src/controller/SubCategory.Controller.js
@@ -1,6 +1,7 @@
 const apiResponse = require("../../utils/apiResponse");
 const { asyncHandler } = require("../../utils/asyncHandler");
 const coustomError = require("../../utils/coustomError");
+const CategoryModel = require("../models/Category.model");
 const subcategoryModel = require("../models/SubCategory.Model");
 const { validateSubCategory } = require("../validation/SubCategory.Validation");
 
@@ -9,6 +10,17 @@ exports.createSubCategory = asyncHandler(async (req, res) => {
   const value = await validateSubCategory(req);
   const subCategory = await subcategoryModel.create(value);
   if (!subCategory) throw new coustomError(500, " subCategory create failed");
+
+  // category model update
+  await CategoryModel.findOneAndUpdate(
+    { _id: value.category },
+    {
+      $push: { subCategory: subCategory._id },
+    },
+    {
+      new: true,
+    }
+  );
   apiResponse.sendSusses(
     res,
     200,
@@ -74,10 +86,18 @@ exports.updateSubCategory = asyncHandler(async (req, res) => {
 exports.deleteSubCategory = asyncHandler(async (req, res) => {
   const { slug } = req.params;
   if (!slug) throw new coustomError(400, "slug missing");
-  const subCategory = await subcategoryModel.findOneAndDelete(
-    { slug: slug }
-  );
+  const subCategory = await subcategoryModel.findOneAndDelete({ slug: slug });
 
+  // category model update
+  await CategoryModel.findOneAndUpdate(
+    { _id: subCategory.category },
+    {
+      $pull: { subCategory: subCategory._id },
+    },
+    {
+      new: true,
+    }
+  );
   if (!subCategory) throw new coustomError(500, " subCategory delete failed");
   apiResponse.sendSusses(
     res,
